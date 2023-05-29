ea0081be289ad98aeccb2cd53fa7fbd9c3fe9c14
Brandon Keepers <brandon@opensoul.org>
2023-05-29 14:35:39 -0400

    Add rss and atom versions of feed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const storify = require ('./storify');
 const readify = require('./readify');
+const jsonfeedToRSS = require('jsonfeed-to-rss')
+const jsonfeedToAtom = require('jsonfeed-to-atom')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', async (req, res) => {
-  const feed = {
+async function feed() {
+  return {
     version: "https://jsonfeed.org/version/1",
     title: "Hacker News Feed",
     home_page_url: "https://feedify.herokuapp.com/",
@@ -15,11 +17,26 @@ app.get('/', async (req, res) => {
     favicon: "https://news.ycombinator.com/favicon.ico",
     items: await Promise.all((await storify()).map(readify))
   }
+}
 
-  res.set('Content-Type', 'application/json');
-  res.send(JSON.stringify(feed, null, 2));
+app.get('/', (req, res) => {
+  res.redirect('/feed.json')
 })
 
+app.get('/feed.json', async (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(JSON.stringify(await feed(), null, 2));
+})
+
+app.get('/feed.rss', async (req, res) => {
+  res.set('Content-Type', 'application/xml');
+  res.send(jsonfeedToRSS(await feed()));
+})
+
+app.get('/feed.atom', async (req, res) => {
+  res.set('Content-Type', 'application/xml');
+  res.send(jsonfeedToAtom(await feed()));
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
