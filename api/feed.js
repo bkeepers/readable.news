9762a9ebb633b2c94b1f5b420d68998c8a1ee4b3
Brandon Keepers <brandon@opensoul.org>
2023-06-10 23:49:28 -0500

    Only cache feed for 15 min

--- api/feed.js
+++ api/feed.js
@@ -4,7 +4,7 @@ import jsonfeedToAtom from 'jsonfeed-to-atom';
 
 const { VERCEL_ENV, VERCEL_URL } = process.env;
 const url = (VERCEL_ENV === 'production' ? `https://` : `http://`) + VERCEL_URL;
-const cacheControl = 'max-age=3600, s-maxage=3600, stale-while-revalidate';
+const cacheControl = 'max-age=900, s-maxage=900, stale-while-revalidate';
 
 async function readify(item) {
   const api = `${url}/api/readable?url=${encodeURIComponent(item.url)}`;
