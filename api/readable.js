ffe7cf0edd53a4043d618836c39a273d30c446a2
Brandon Keepers <brandon@opensoul.org>
2023-06-12 11:58:34 -0500

    Use headless.io to fetch front-end rendered content

--- api/readable.js
+++ api/readable.js
@@ -1,60 +1,40 @@
-import { Readability } from '@mozilla/readability';
-import ogs from 'open-graph-scraper';
-import { parseHTML } from 'linkedom';
-import truncate from 'truncate-html';
+import * as browsers from '../lib/browsers/index.js'
+import formats from '../lib/formats/index.js'
 
-// Cache for 1 hour
-const cacheControl = 'max-age=3600, s-maxage=3600, stale-while-revalidate';
+// Cache for 12 hours
+const age = 60 * 60 * 12;
+const cacheControl = `max-age=${age}, s-maxage=${age}, stale-while-revalidate`;
 
-function resolveUrl(url, base) {
-  if(!url) return;
+async function readable (url, browser = browsers.node) {
+  const { contentType, text } = await browser(url);
 
-  return url && new URL(url, base).toString();
-}
-
-const formats = {
-  'text/html': async (res) => {
-    const html = await res.text()
-    const article = new Readability(parseHTML(html, { url: res.url }).window.document).parse();
-    const og = (await ogs({ html })).result
-
-    return {
-      content_html: truncate(article.content, 4000, { byWords: true }),
-      excerpt: article.excerpt,
-      image: resolveUrl(og?.ogImage?.[0]?.url, res.url),
-      authors: [{
-        name: article.siteName || og.ogSiteName || article.byline,
-        url: res.url,
-        avatar: resolveUrl(og.favicon, res.url)
-      }]
-    }
-  },
-  'text/plain': async (res) => {
-    return {
-      content_text: await res.text(),
-      author: {
-        url: res.url
-      }
-    }
+  if(!formats[contentType]) {
+    throw new Error(`Unknown Content-Type: ${contentType}`)
   }
-}
 
-async function readable (item) {
-  if(!item.url) return item
   try {
-    const res = await fetch(item.url);
-    const contentType = res.headers.get('Content-Type').split(';')[0];
-    if(!formats[contentType]) throw new Error(`Unknown Content-Type for ${item.url}: ${contentType}`)
-    return { ...item, ...await formats[contentType](res) || {} }
+    return await formats[contentType]({ text, url })
   } catch (err) {
-    console.error(`Failed to parse ${item.url}`, err)
-    return item
+    console.error(err)
+    if(browser === browsers.headless) {
+      throw err
+    } else {
+      console.log(`Trying to parse ${url} with headless browser`)
+      return await readable(url, browsers.headless)
+    }
   }
 }
-
 
 export default async function handler(req, res) {
-  res.setHeader('Cache-Control', cacheControl)
-  res.setHeader('Content-Type', 'application/json');
-  return res.json(await readable({ url: req.query.url }));
+  const { url } = req.query;
+
+  try {
+    const item = await readable(url)
+    res.setHeader('Cache-Control', cacheControl)
+    res.setHeader('Content-Type', 'application/json');
+    res.json(item)
+  } catch (err) {
+    console.error(`Failed to parse ${url}`, err)
+    res.status(422).json({ url, error: err.message })
+  }
 }
