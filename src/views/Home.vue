dd4be9d6631942a0a861eaa433ff8116d711f15f
Brandon Keepers <brandon@opensoul.org>
2023-06-19 13:34:26 -0400

    Reduce padding on mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ import Feed from '../components/Feed.vue'
 </script>
 
 <template>
-  <div class="container-xl mx-auto p-8">
+  <div class="container-xl mx-auto p-4 md:p-8 lg:p-8">
     <Feed />
   </div>
 </template>
