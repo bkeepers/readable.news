a0018c8e24e6284975291d0b4600941edfbcd06b
Brandon Keepers <brandon@opensoul.org>
2023-06-21 12:32:43 -0400

    Remove KV since it's currently broken

--- api/readable.js
+++ api/readable.js
@@ -1,6 +1,5 @@
 import * as browsers from '../lib/browsers/index.js'
 import formats from '../lib/formats/index.js'
-import { kv } from "@vercel/kv";
 
 // Cache for 12 hours
 const age = 60 * 60 * 12;
@@ -19,13 +18,10 @@ async function readable (url, browser = browsers.node) {
 
   try {
     const item = await formats[contentType]({ text, url })
-    await kv.set(key(url), item, { ex: age })
     return item
   } catch (err) {
     console.error(err)
     if(browser === browsers.headless) {
-      // Store empty object to prevent future attempts
-      await kv.set(key(url), { failed: true }, { ex: age })
       throw err
     } else {
       console.log(`Trying to parse ${url} with headless browser`)
@@ -38,10 +34,9 @@ export default async function handler(req, res) {
   const { url } = req.query;
 
   console.log(`Making readable ${url}`)
-  kv.del(url)
 
   try {
-    const item = (await kv.get(key(url))) || (await readable(url))
+    const item = await readable(url)
     res.setHeader('Cache-Control', cacheControl)
     res.setHeader('Content-Type', 'application/json');
     res.json(item)
