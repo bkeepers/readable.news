2494913f04348128eef58cc631c1c0d9d54da9d3
Brandon Keepers <brandon@opensoul.org>
2024-09-15 22:19:21 -0400

    Record failures for domains/browser combinations

--- api/readable.js
+++ api/readable.js
@@ -1,5 +1,6 @@
 import browsers from '../lib/browsers/index.js'
 import formats from '../lib/formats/index.js'
+import { kv } from '@vercel/kv'
 
 // Cache for 12 hours
 const age = 60 * 60 * 12
@@ -8,7 +9,17 @@ const cacheControl = `public, max-age=${age}, s-maxage=${age}, stale-while-reval
 async function readable (url) {
   let lastError = null
 
+  const domain = new URL(url).hostname
+
   for (const browser of browsers) {
+    const key = `domain_failures:${domain}`
+    const failures = await kv.hget(key, browser.name) || 0
+
+    if (failures >= 3) {
+      // Too many failures fetching this domain with this browser
+      continue
+    }
+
     try {
       console.debug(`Getting ${url} with ${browser.name}`)
       const { contentType, text } = await browser.request(url)
@@ -20,6 +31,9 @@ async function readable (url) {
       const item = await formats[contentType]({ text, url })
       return item
     } catch (err) {
+      // Record the failure for this domain
+      kv.hincrby(key, browser.name, 1)
+      kv.expire(key, 60 * 60 * 24) // 24 hours
       console.error(`Failed to request ${url} with ${browser.name}.`, err)
       lastError = err
     }
